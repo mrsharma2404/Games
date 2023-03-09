92847748e7ab8477a77f664afb24e87cda2b9cee
Rohit Sharma <mrsharma2404@gmail.com>
2023-03-10 02:52:42 +0530

    mouse is running horizontally only

---
+++ app.js
@@ -0,0 +1,88 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d')
+canvas.width = 600;
+canvas.height = 720;
+
+class InputHandler {
+  constructor() {
+    this.keys = []
+    window.addEventListener('keydown', e => {
+      console.log({ e })
+      if ((e.key === 'ArrowDown' || e.key === 'ArrowUp' || e.key === 'ArrowLeft' || e.key === 'ArrowRight')
+        && this.keys.indexOf(e.key) === -1) {
+        this.keys.push(e.key)
+      }
+    })
+    window.addEventListener('keyup', e => {
+      if (e.key === 'ArrowDown' || e.key === 'ArrowUp' || e.key === 'ArrowLeft' || e.key === 'ArrowRight') {
+        this.keys.splice(this.keys.indexOf(e.key), 1)
+      }
+    })
+  }
+}
+
+class Player {
+  constructor(gameWidth, gameHeight) {
+    this.gameWidth = gameWidth;
+    this.gameHeight = gameHeight;
+    this.width = 200;
+    this.height = 200;
+    this.x = 0;
+    this.y = this.gameHeight - this.height;
+    this.image = document.getElementById('playerImage')
+    this.frameX = 0;
+    this.frameY = 0;
+    this.speed = 1;
+
+  }
+  draw(context) {
+    context.fillStyle = 'white'
+    context.fillRect(this.x, this.y, this.width, this.height)
+    context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x, this.y, this.width, this.height)
+    //context.drawImage(image, sourceX, sourceY, sorceWidth, sourceHeight, xPosition, yPosition, width, height)
+  }
+  update(input) {
+    //horizontal movement 
+    this.x += this.speed;
+    if (input.keys.indexOf('ArrowRight') > -1) {
+      this.speed = 5;
+    }
+    else if (input.keys.indexOf('ArrowLeft') > -1) {
+      this.speed = -5;
+    }
+    else {
+      this.speed = 0;
+    }
+    if (this.x < 0) this.x = 0;
+    else if (this.x > this.gameWidth - this.width) this.x = this.gameWidth - this.width
+  }
+
+}
+
+class Backgrounf {
+
+}
+
+class Enemy {
+
+}
+
+function handleEnemies() {
+
+}
+function displayStatusText() {
+
+}
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  player.draw(ctx);
+  player.update(input);
+  requestAnimationFrame(animate)
+}
+
+const input = new InputHandler()
+const player = new Player(canvas.width, canvas.height)
+// player.draw(ctx);
+// update();
+
+animate();
